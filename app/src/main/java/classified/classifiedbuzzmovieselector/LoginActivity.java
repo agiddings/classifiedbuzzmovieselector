ae03d1432e5ecf44cc6051e60c78efb63a467887
CABowers <cole.a.bowers@gmail.com>
2016-02-06 11:56:27 -0500

    Added comments for how to switch activites

--- app/src/main/java/classified/classifiedbuzzmovieselector/LoginActivity.java
+++ app/src/main/java/classified/classifiedbuzzmovieselector/LoginActivity.java
@@ -1,6 +1,7 @@
 package classified.classifiedbuzzmovieselector;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,20 +23,31 @@ public class LoginActivity extends AppCompatActivity {
         EditText emailBox = (EditText) findViewById(R.id.loginEmail);
         EditText passBox = (EditText) findViewById(R.id.loginPassword);
 
+        CharSequence text;
         if (manager.handleLoginRequest(emailBox.getText().toString(), passBox.getText().toString())) {
             //TODO Go to postLogin Screen
+            //This method might go in the postLogin screen with the following:
+            // Log.d("**MYAPP**", "Login button pressed!");
+            //Intent intent = new Intent(this, LoginActivity.class);
+            //startActivity(intent);
+
+            text = "Login Successful";
+
         } else {
-            CharSequence text;
-            text = "Login Failure!";
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast t = Toast.makeText(context, text, duration);
-            t.show();
+            text = "Login Failed";
         }
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast t = Toast.makeText(context, text, duration);
+        t.show();
     }
 
     public void onRegistrationLinkPressed(View v){
         Log.d("LOGIN ACTIVITY", "Registration Link Pressed");
         //TODO GO to registration screen
+        //This method might go in the registration screen with the following:
+        //Log.d("**MYAPP**", "Registration Link pressed!");
+        //Intent intent = new Intent(this, LoginActivity.class);
+        //startActivity(intent);
     }
 }
