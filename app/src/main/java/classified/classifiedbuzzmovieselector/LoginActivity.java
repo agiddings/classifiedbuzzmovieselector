ff7cb68f3b7f165b388539fbdba8ced8d37bc3da
CABowers <cole.a.bowers@gmail.com>
2016-02-06 11:26:54 -0500

    Added fuctionality

--- app/src/main/java/classified/classifiedbuzzmovieselector/LoginActivity.java
+++ app/src/main/java/classified/classifiedbuzzmovieselector/LoginActivity.java
@@ -1,7 +1,12 @@
 package classified.classifiedbuzzmovieselector;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginButtonPressed(View v) {
+        Log.d("LOGIN ACTIVITY", "Login Button Pressed");
+        UserManager manager = new UserManager();
+        EditText emailBox = (EditText) findViewById(R.id.loginEmail);
+        EditText passBox = (EditText) findViewById(R.id.loginPassword);
+
+        if (manager.handleLoginRequest(emailBox.getText().toString(), passBox.getText().toString())) {
+            //TODO Go to postLogin Screen
+        } else {
+            CharSequence text;
+            text = "Login Failure!";
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast t = Toast.makeText(context, text, duration);
+            t.show();
+        }
     }
 }
