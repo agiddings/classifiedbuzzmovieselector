d32864a518009e05a3b95bdcb3d27343a99bb200
Stuev <swulber2015@gmail.com>
2016-02-06 10:45:41 -0500

    Make InputsDoNotMatchException

---
+++ app/src/main/java/classified/classifiedbuzzmovieselector/InputsDoNotMatchException.java
@@ -0,0 +1,15 @@
+package classified.classifiedbuzzmovieselector;
+
+/**
+ * Created by steven on 2/6/16.
+ */
+public class InputsDoNotMatchException extends Exception {
+
+    public InputsDoNotMatchException() {
+        super();
+    }
+
+    public InputsDoNotMatchException(String message) {
+        super(message);
+    }
+}
