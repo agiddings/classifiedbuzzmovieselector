de1cd67f8f21339e1b044da5518bfe3c9a0af58e
justeenlee <justeenlee@gatech.edu>
2016-02-08 12:07:08 -0500

    Added hardcoded user: name = John email = user@gmail.com password = pass

--- app/src/main/java/classified/classifiedbuzzmovieselector/model/UserManager.java
+++ app/src/main/java/classified/classifiedbuzzmovieselector/model/UserManager.java
@@ -9,13 +9,18 @@ import classified.classifiedbuzzmovieselector.model.User;
 import classified.classifiedbuzzmovieselector.model.UserAlreadyExistsException;
 
 /**
- * Created by steven on 2/5/16.
+ * Created by Steven on 2/5/16.
  */
 public class UserManager {
     private static Map<String, User> users = new HashMap<>();
+    private User tempUser;
 
-    public UserManager() {} // empty for now
+    public UserManager() {
+        //create a temporary user for M3 demo
+        tempUser = new User("John", "user@gmail.com", "pass");
+    }
 
+    //what is the method below trying to do? - Justeen
     public User findUserByEmail(String email) {
         return users.get(email);
     }
@@ -28,7 +33,7 @@ public class UserManager {
             throw new IllegalArgumentException("The email field is invalid");
         } else if (password == null) {
             throw new IllegalArgumentException("The password field is invalid");
-        } else if (users.get(email) != null) {
+        } else if (users.containsKey(email)) {
             throw new UserAlreadyExistsException(email + " is already registered");
         }
         User user = new User(name, email, password);
