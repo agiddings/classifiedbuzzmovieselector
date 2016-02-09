d55efad75d4ca5202a7b396a9d8d862f3082c3b7
annalaura <annalauradavenport@gmail.com>
2016-02-08 19:46:05 -0500

    logout button redirects to login page

--- app/src/main/java/classified/classifiedbuzzmovieselector/controllers/PostLoginActivity.java
+++ app/src/main/java/classified/classifiedbuzzmovieselector/controllers/PostLoginActivity.java
@@ -1,7 +1,9 @@
 package classified.classifiedbuzzmovieselector.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import classified.classifiedbuzzmovieselector.R;
 
@@ -11,5 +13,10 @@ public class PostLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_login);
+    }
+
+    public void onLogoutButtonClicked(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
