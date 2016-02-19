4e7cf66bca071d59aec21b5f6bcf44dc9ec15109
Stuev <swulber2015@gmail.com>
2016-02-19 15:36:51 -0500

    Remove checks on p less bulky

---
+++ app/src/main/java/classified/classifiedbuzzmovieselector/model/MovieManager.java
@@ -0,0 +1,18 @@
+package classified.classifiedbuzzmovieselector.model;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by steven on 2/19/16.
+ */
+public class MovieManager {
+    private static List<Movie> movies;
+    private static final Gson gson = new Gson();
+
+    public MovieManager (String json) {
+
+    }
+}
