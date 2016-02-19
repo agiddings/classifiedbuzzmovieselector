425ef2c66ba06619e8a73c0d495960cad320fe2d
annalaura <annalauradavenport@gmail.com>
2016-02-19 15:20:31 -0500

    unable to login

---
+++ app/src/main/java/classified/classifiedbuzzmovieselector/controllers/UnableLoginActivity.java
@@ -0,0 +1,20 @@
+package classified.classifiedbuzzmovieselector.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import classified.classifiedbuzzmovieselector.R;
+
+/**
+ * Created by anna on 2/19/16.
+ */
+public class UnableLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unable_to_login);
+    }
+}
