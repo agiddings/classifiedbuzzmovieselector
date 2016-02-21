82fb3dadfb27a097b0f45ec35ae92201c69e21d5
justeen <justeenlee@gatech.edu>
2016-02-20 19:16:57 -0500

    Added listItemActivity.java

---
+++ app/src/main/java/classified/classifiedbuzzmovieselector/controllers/listItemActivity.java
@@ -0,0 +1,12 @@
+package classified.classifiedbuzzmovieselector.controllers;
+
+/**
+ * Created by Zhendong(Justeen) on 2/20/16.
+ */
+public class listItemActivity {
+    private String listTitle;
+    private int year;
+
+
+
+}
