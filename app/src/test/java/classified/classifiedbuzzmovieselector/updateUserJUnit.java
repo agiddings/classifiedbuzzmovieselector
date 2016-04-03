efcd9a7e850f9cedb7412cfba7ad039cf95a17ff
annalaura <annalauradavenport@gmail.com>
2016-04-03 17:31:57 -0400

    update Junit

---
+++ app/src/test/java/classified/classifiedbuzzmovieselector/updateUserJUnit.java
@@ -0,0 +1,54 @@
+package classified.classifiedbuzzmovieselector;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import classified.classifiedbuzzmovieselector.model.Exceptions.UserDoesNotExistException;
+import classified.classifiedbuzzmovieselector.model.Exceptions.InvalidEmailException;
+import classified.classifiedbuzzmovieselector.model.Exceptions.InvalidMajorException;
+
+import classified.classifiedbuzzmovieselector.model.UserManager;
+
+/**
+ * Created by Anna Laura on 4/1/2016.
+ */
+public class updateUserJUnit {
+
+    private UserManager anna;
+    @Before
+    public void makeUser() {
+        if (anna == null) {
+             anna = new UserManager();
+        }
+        try {
+            anna.addUser("Anna", "a@gmail.com", "password");
+        }
+        catch(Exception e){
+
+        }
+
+    }
+
+    @Test(expected = InvalidEmailException.class)
+    public void testUpdateUserInvalidEmail1()  throws UserDoesNotExistException, InvalidEmailException, InvalidMajorException {
+        UserManager.updateUser("a@gmail.com", "Anna", "a@gmail.com" , "password" , "Computer Science", "2nd year");
+    }
+
+    @Test(expected = InvalidEmailException.class)
+    public void testUpdateUserInvalidEmail2()  throws UserDoesNotExistException, InvalidEmailException, InvalidMajorException {
+        UserManager.updateUser("a@gmail.com", "Anna", "asdf" , "password" , "Computer Science", "2nd year");
+    }
+
+    @Test(expected = InvalidMajorException.class)
+    public void testUpdateUserInvalidMajor() throws UserDoesNotExistException, InvalidEmailException, InvalidMajorException {
+        UserManager.updateUser("a@gmail.com", "Anna", "a@gmail.com" , "password" , "Fashion", "2nd year");
+    }
+
+    @Test(expected = UserDoesNotExistException.class)
+    public void testUpdateUserAlreadyExists() throws UserDoesNotExistException, InvalidEmailException, InvalidMajorException{
+        UserManager.updateUser("asf@gmail.com", "Anna", "sfkn@gmail.com" , "password" , "Computer Science", "2nd year");
+        assert(UserManager.findUserByEmail("a@gmail.com").equals(UserManager.findUserByEmail("asf@mail.com")));
+    }
+
+}
+u
