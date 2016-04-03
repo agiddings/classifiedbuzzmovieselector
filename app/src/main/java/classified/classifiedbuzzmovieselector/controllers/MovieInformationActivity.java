c0bb5bdce48fa2ab9bd80377cf85af2ddc78fa81
Stuev <swulber2015@gmail.com>
2016-04-03 15:52:01 -0400

    Add database functionality to the app

--- app/src/main/java/classified/classifiedbuzzmovieselector/controllers/MovieInformationActivity.java
+++ app/src/main/java/classified/classifiedbuzzmovieselector/controllers/MovieInformationActivity.java
@@ -16,8 +16,12 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import org.w3c.dom.Text;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import classified.classifiedbuzzmovieselector.R;
@@ -85,6 +89,16 @@ public class MovieInformationActivity extends AppCompatActivity {
                 UserRatingManager.addUserRating(r);
                 listOfRatings.add(r);
                 myAdapter.notifyDataSetChanged();
+            }
+            Gson gson = new Gson();
+            UserRatingManager userRatingManager = new UserRatingManager();
+            String json = gson.toJson(userRatingManager.getUserRatings());
+            try {
+                FileOutputStream fos = openFileOutput("ratings.txt", Context.MODE_PRIVATE);
+                fos.write(json.getBytes());
+                fos.close();
+            } catch (IOException e) {
+                Log.d("MOVIE_INFORMATION_ACT", e.toString());
             }
 
         } catch (Exception e) {
